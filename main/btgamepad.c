b681f74ab7904d38e7431330fb044d27b1185d8d
fluffymadness <fluffymadness@posteo.de>
2019-04-21 21:15:25 +0200

    changes

--- main/btgamepad.c
+++ main/btgamepad.c
@@ -15,105 +15,10 @@ static btstack_packet_callback_registration_t hci_event_callback_registration;
 static uint16_t hid_cid;
 static uint16_t pairing_state;
 
-static uint8_t hid_service_buffer[450];
-static uint8_t device_id_sdp_service_buffer[200];
+static uint8_t hid_service_buffer[900];
+static uint8_t device_id_sdp_service_buffer[400];
 static uint8_t l2cap_sdp_service_buffer[600];
 
-/* USB Pro Controller Report Map
-const uint8_t reportMap[] = {
-0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
-0x15, 0x00,        // Logical Minimum (0)
-0x09, 0x04,        // Usage (Joystick)
-0xA1, 0x01,        // Collection (Application)
-0x85, 0x30,        //   Report ID (48)
-0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
-0x05, 0x09,        //   Usage Page (Button)
-0x19, 0x01,        //   Usage Minimum (0x01)
-0x29, 0x0A,        //   Usage Maximum (0x0A)
-0x15, 0x00,        //   Logical Minimum (0)
-0x25, 0x01,        //   Logical Maximum (1)
-0x75, 0x01,        //   Report Size (1)
-0x95, 0x0A,        //   Report Count (10)
-0x55, 0x00,        //   Unit Exponent (0)
-0x65, 0x00,        //   Unit (None)
-0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x05, 0x09,        //   Usage Page (Button)
-0x19, 0x0B,        //   Usage Minimum (0x0B)
-0x29, 0x0E,        //   Usage Maximum (0x0E)
-0x15, 0x00,        //   Logical Minimum (0)
-0x25, 0x01,        //   Logical Maximum (1)
-0x75, 0x01,        //   Report Size (1)
-0x95, 0x04,        //   Report Count (4)
-0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x75, 0x01,        //   Report Size (1)
-0x95, 0x02,        //   Report Count (2)
-0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x0B, 0x01, 0x00, 0x01, 0x00,  //   Usage (0x010001)
-0xA1, 0x00,        //   Collection (Physical)
-0x0B, 0x30, 0x00, 0x01, 0x00,  //     Usage (0x010030)
-0x0B, 0x31, 0x00, 0x01, 0x00,  //     Usage (0x010031)
-0x0B, 0x32, 0x00, 0x01, 0x00,  //     Usage (0x010032)
-0x0B, 0x35, 0x00, 0x01, 0x00,  //     Usage (0x010035)
-0x15, 0x00,        //     Logical Minimum (0)
-0x27, 0xFF, 0xFF, 0x00, 0x00,  //     Logical Maximum (65534)
-0x75, 0x10,        //     Report Size (16)
-0x95, 0x04,        //     Report Count (4)
-0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0xC0,              //   End Collection
-0x0B, 0x39, 0x00, 0x01, 0x00,  //   Usage (0x010039)
-0x15, 0x00,        //   Logical Minimum (0)
-0x25, 0x07,        //   Logical Maximum (7)
-0x35, 0x00,        //   Physical Minimum (0)
-0x46, 0x3B, 0x01,  //   Physical Maximum (315)
-0x65, 0x14,        //   Unit (System: English Rotation, Length: Centimeter)
-0x75, 0x04,        //   Report Size (4)
-0x95, 0x01,        //   Report Count (1)                               Data,Var,Abs,No Wrap,Linear,Preferred State,Null State
-0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x05, 0x09,        //   Usage Page (Button)
-0x19, 0x0F,        //   Usage Minimum (0x0F)
-0x29, 0x12,        //   Usage Maximum (0x12)
-0x15, 0x00,        //   Logical Minimum (0)
-0x25, 0x01,        //   Logical Maximum (1)
-0x75, 0x01,        //   Report Size (1)
-0x95, 0x04,        //   Report Count (4)
-0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x34,        //   Report Count (52)
-0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x06, 0x00, 0xFF,  //   Usage Page (Vendor Defined 0xFF00)
-0x85, 0x21,        //   Report ID (33)
-0x09, 0x01,        //   Usage (0x01)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x85, 0x81,        //   Report ID (-127)
-0x09, 0x02,        //   Usage (0x02)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-0x85, 0x01,        //   Report ID (1)
-0x09, 0x03,        //   Usage (0x03)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x91, 0x83,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-0x85, 0x10,        //   Report ID (16)
-0x09, 0x04,        //   Usage (0x04)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x91, 0x83,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-0x85, 0x80,        //   Report ID (-128)
-0x09, 0x05,        //   Usage (0x05)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x91, 0x83,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-0x85, 0x82,        //   Report ID (-126)
-0x09, 0x06,        //   Usage (0x06)
-0x75, 0x08,        //   Report Size (8)
-0x95, 0x3F,        //   Report Count (63)
-0x91, 0x83,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-0xC0, // End Collection
-  };
-*/
 //Bluetooth Pro Controller report map
 const uint8_t reportMap[] = {
     0x05, 0x01, 
@@ -223,19 +128,16 @@ typedef struct {
 gamepad_report_t;
 
 static gamepad_report_t joy;
-uint8_t command1[] = {0xA1, 0x3F, 0x00, 0x00, 0x05, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-uint8_t command2[] = {0xA1, 0x3F, 0x00, 0x00, 0x05, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-uint8_t command3[] = {0xA1, 0x3F, 0x00, 0x00, 0x08, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-uint8_t command4[] = {0xA1, 0x3F, 0x00, 0x00, 0x08, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-uint8_t command5[] = {0xA1, 0x3F, 0x04, 0x00, 0x08, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-uint8_t command6[] = {0xA1, 0x3F, 0x00, 0x00, 0x08, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80};
-static uint8_t reply02[] = {0xa1, 0x21, 0x00, 0x40, 0x00, 0x00, 0x00, 0xe6, 0x27, 0x78, 0xab, 0xd7, 0x76, 0x00, 0x82, 0x02, 0x03, 0x48, 0x03, 0x02, 0xD8, 0xA0, 0x1D, 0x40, 0x6B, 0x22, 0x03, 0x01, 0x00 , 0x00 , 0x00 , 0x00  , 0x00 , 0x00 , 0x00 , 0x00  , 0x00 , 0x00 , 0x00 , 0x00  , 0x00 , 0x00 , 0x00 , 0x00
-, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 };
 
-static uint8_t reply01[] = {0xA1, 0x21, 0x1f, 0x8e, 0x00, 0x00, 0x00, 0x00, 0x08, 0x80, 0x00, 0x00, 0x00, 0x01, 0x82, 0x02, 0x03, 0x91, 0x01, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t command1[] = {0xA1, 0x3F, 0x00, 0x00, 0x08, 0xA0, 0x80, 0x2F, 0x87, 0x00, 0x87, 0xFF, 0x84};
+uint8_t command2[] = {0xA1, 0x3F, 0x00, 0x00, 0x08, 0xB0, 0x80, 0x3F, 0x87, 0xF0, 0x86, 0xFF, 0x84};
+static uint8_t reply02[] = {0xA1, 0x21, 0x1f, 0x8e, 0x00, 0x00, 0x00, 0x00, 0x08, 0x80, 0x00, 0x00, 0x00, 0x01, 0x82, 0x02, 0x03, 0x91, 0x01, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
+static void send_reply02(){
+    hid_device_send_interrupt_message(hid_cid, &reply02[0], sizeof(reply02));
+}
 
-static void send_report_joystick2(){
+static void send_report_joystick_full(){
     //0x30 is the report id , 0xa1 indicates hid data
    // joy.HAT = 0x02;
     joy.timer++;
@@ -278,26 +180,6 @@ static void send_report_joystick(){
     //send report
     hid_device_send_interrupt_message(hid_cid, &report[0], sizeof(report));
 }
-static void send_data(){
-   
-        if(pairing_state == 0)
-            hid_device_send_interrupt_message(hid_cid, &command1[0], sizeof(command1));
-            //hid_device_send_interrupt_message(hid_cid, &command1[0], sizeof(command1));
-        if(pairing_state == 1)
-            hid_device_send_interrupt_message(hid_cid, &command2[0], sizeof(command2));
-        if(pairing_state == 3)
-            hid_device_send_interrupt_message(hid_cid, &command3[0], sizeof(command3));
-        if(pairing_state == 4)
-            hid_device_send_interrupt_message(hid_cid, &command4[0], sizeof(command4));
-        if(pairing_state == 5)
-            hid_device_send_interrupt_message(hid_cid, &command5[0], sizeof(command5));
-        if(pairing_state == 6)
-            hid_device_send_interrupt_message(hid_cid, &command6[0], sizeof(command6));
-        if(pairing_state >= 7)
-            //send_report_joystick2();
-            hid_device_send_interrupt_message(hid_cid, &command6[0], sizeof(command6));
-        pairing_state++;
-}
 
 static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t * packet, uint16_t packet_size){
     UNUSED(channel);
@@ -337,10 +219,16 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t * pack
                         break;
                     case HID_SUBEVENT_CAN_SEND_NOW:  
                         if(hid_cid!=0){
-
-                          send_report_joystick();
-
-                          hid_device_request_can_send_now_event(hid_cid);
+                            
+                            if(pairing_state == 0)
+                                hid_device_send_interrupt_message(hid_cid, &command1[0], sizeof(command1));
+                            if(pairing_state == 1)
+                                hid_device_send_interrupt_message(hid_cid, &command2[0], sizeof(command2));
+                            else{
+                                send_report_joystick();
+                            }
+                            pairing_state++;
+                            hid_device_request_can_send_now_event(hid_cid);
                         }
                         break;
                     default:
